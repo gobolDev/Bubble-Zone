0871f30c9b2ad5ea10040cd1ba4d140e037182f2
gobolDev <47417397+gobolDev@users.noreply.github.com>
2019-03-04 17:38:19 +0100

    Player, Level Scenes.
    Folder > Player.
    Script is used in Level Scenes.

---
+++ Scripts/Player/BulletCollider.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+/// <summary>
+/// Handles collisions between Wall. This script is attached to Bullet Prefab.
+/// </summary>
+public class BulletCollider : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        switch (other.tag)
+        {
+            case "Wall":
+                gameObject.SetActive(false);    // Set this Bullet activity to false.
+                break;
+            case "Breakable":
+                gameObject.SetActive(false);    // Set this Bullet activity to false.
+                Destroy(other.gameObject);      // Destroy other gameObject.
+                break;
+        }
+    }
+}
