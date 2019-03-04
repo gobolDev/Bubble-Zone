19745efc0ee8b85bc0442bdff61d1b3868c725ab
gobolDev <47417397+gobolDev@users.noreply.github.com>
2019-03-04 17:52:47 +0100

    Handler, CampainMap & Level Scenes.
    Folder > Handler.
    Script is used in CampanMap & Level Scenes.

---
+++ Scripts/Handlers/AdWatcher.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+/// <summary>
+/// Handles the Advertisement system in the game. This script is attahed to AdWatcher Panel.
+/// </summary>
+public class AdWatcher : MonoBehaviour
+{
+    public void WatchAnAd()     // Called when user agreed to press a button to watch an Ad.
+    {
+        DataCtrl.instance._data.Life = DataCtrl.instance._data.Life + 2;    // Add 2 more lifes to player.
+        DataCtrl.instance.SaveData();                                       // Saves the given 2 lifes.
+        gameObject.SetActive(false);                                        // Hides AdWatcher panel.
+    }
+
+    public void CancelButton()  // Called when user disagreed to press a button to watch an Ad.
+    {
+        gameObject.SetActive(false);    // Hides AdWatcher panel.
+    }
+}
+
+// Unfinished work! A script is under construction!
