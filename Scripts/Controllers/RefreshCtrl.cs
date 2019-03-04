723a443a5ceab4583065385e1a7c313d10093588
Никола Гобељић <47417397+gobolDev@users.noreply.github.com>
2019-03-04 19:56:49 +0100

    Controller, CampainMap.
    Folder > Controller.
    Script used in CampainMap Scene.

---
+++ Scripts/Controllers/RefreshCtrl.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+/// <summary>
+/// Updates DataCtrl so that it provides the most recent data. This script is used in CampainMap Scene.
+/// </summary>
+public class RefreshCtrl : MonoBehaviour
+{
+	private void Awake ()
+    {
+        DataCtrl.instance.RefreshData();
+	}
+}
