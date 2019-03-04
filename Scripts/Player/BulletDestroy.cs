49f22ab9b780ad5d1424f02148df622bfbd48fd9
gobolDev <47417397+gobolDev@users.noreply.github.com>
2019-03-04 17:40:04 +0100

    Player, Level Scenes.
    Folder > Player.
    Script is used in Level Scenes.

---
+++ Scripts/Player/BulletDestroy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+/// <summary>
+/// Hides bullets. This script is attached to bullet Prefab.
+/// </summary>
+public class BulletDestroy : MonoBehaviour
+{
+    void OnEnable ()    // Called when Bullet spawns.
+    {
+        Invoke("Hide", 1f); // Invokes a Hide Method.
+	}
+
+    public void Hide()  // Called from Enabled, or, when Bullet hit the Bubble or the Cilling.
+    {
+        gameObject.SetActive(false);    // Hides the gameObject.
+    }
+
+    void OnDisable()    // Called when Bullet is Disabled.
+    {   
+        CancelInvoke(); // Cancels Invoke method before Script is being Disabled for this gameObject. 
+    }
+}
