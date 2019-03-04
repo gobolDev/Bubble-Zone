c562ca0e862145c4e25608d0931f321dbb43306d
Никола Гобељић <47417397+gobolDev@users.noreply.github.com>
2019-03-04 18:09:09 +0100

    Controller, CampainMap.
    Folder > Controller.
    Script is used in CampainMap Scene.

---
+++ Scripts/Controllers/BtnSaveController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+/// <summary>
+/// This script is used ONLY for saving data during EARLY TESTING! It will be removed in future.
+/// </summary>
+public class BtnSaveController : MonoBehaviour
+{
+    public void SaveData()  // Called when Button "Save" is pressed.
+    {
+        DataCtrl.instance.SaveData();   // Saved the current data information to DB!
+    }
+}
