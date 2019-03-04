afee111780489f7120e89c32eadf636bfd39465a
Никола Гобељић <47417397+gobolDev@users.noreply.github.com>
2019-03-04 21:18:25 +0100

    Handler, Level Scenes.
    Folder > Handler.
    Script is used in Level Scenes.

---
+++ Scripts/Handlers/SceneChanger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+/// <summary>
+/// This script handles Scene changing. This script can be found at most buttons that change Scene.
+/// </summary>
+public class SceneChanger : MonoBehaviour
+{
+    public string sceneName;    // Variable of string containing the name of the Scene.
+
+    public void LoadScene()     // Called when player clicks the button for Menu.
+    {
+        SceneManager.LoadScene(sceneName);  // Load sceneName scene.
+    }
+
+    public void RestartScene()  // Called when player clicks the button to replay the current level.
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);   // Restart this active Scene.
+    }
+}
