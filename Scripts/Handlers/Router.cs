103166fe2f5aef5b88c8e3f5053ec7d707dc14fe
Никола Гобељић <47417397+gobolDev@users.noreply.github.com>
2019-03-04 21:15:31 +0100

    Handler, Level Scenes.
    Folder > Handler.
    Script is used in Level Scenes.

---
+++ Scripts/Handlers/Router.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+/// <summary>
+/// This script handles Pause/Resume system. This script is found in level Scenes,
+/// </summary>
+public class Router : MonoBehaviour
+{
+    private string sceneName;   // Var. of string containing the name of Scene.
+
+    public void ShowPausePanel()    // Called when Pause Button is pressed.
+    {
+        GameCtrl.instance.ShowPausePanel(); // Pause the game.
+    }
+
+    public void HidePausePanel()    // Called when Resume Button is pressed.
+    {
+        GameCtrl.instance.HidePausePanel(); // Resume the game.
+    }
+
+    public void LoadScene(string sceneName) // Called when Menu Button is pressed.
+    {
+        SceneManager.LoadScene(sceneName);  // Load sceneName scene.
+    }
+}
