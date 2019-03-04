4655ca840a6e346539dd9dd44f9ecb6f22934533
Никола Гобељић <47417397+gobolDev@users.noreply.github.com>
2019-03-04 21:21:14 +0100

    Handler, PlayMenu.
    Folder > Handler.
    Script is used in PlayerMenu Scene.

---
+++ Scripts/Handlers/Switch.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+/// <summary>
+/// This script handles Scene changing in PlayMenu Scene.
+/// </summary>
+public class Switch : MonoBehaviour
+{
+    public void CampainLoad()   // Called when Campain Button is pressed.
+    {
+        SceneManager.LoadScene("CampainMap");   // Loads the CampainMap Scene.
+    }
+
+    public void BackLoad()      // Called when Back Button is pressed.
+    {
+        SceneManager.LoadScene("MainMenu");     // Loads the MainMenu Scene.
+    }
+}
